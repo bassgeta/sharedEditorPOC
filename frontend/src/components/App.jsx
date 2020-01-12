5f06229c2ff3380f6e0efbfd57cfed9db05a92da
Kerry Mahne <kerry.mahne@tretton37.com>
2020-01-12 12:10:41 +0100

    frontend initial render

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import styled from "styled-components"
+
+const Test = styled.div`
+  width: 200px;
+  height: 100px;
+
+  background-color: red;
+`
+const App = () => {
+  return (
+    <Test />
+  )
+}
+
+export default App
+
