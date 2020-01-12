eb3586ee27e06b027feb82dd994fa719260d62b2
Kerry Mahne <kerry.mahne@tretton37.com>
2020-01-12 12:20:38 +0100

    backend initial connection

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const WebSocket = require("ws")
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", ws => {
+  console.log("Connection opened!")
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+  });
+
+  ws.send("something");
+});
